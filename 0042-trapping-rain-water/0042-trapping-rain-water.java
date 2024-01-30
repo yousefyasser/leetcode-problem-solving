b8e692fdab22b8b9830ccdce555c98cc5fc3bec6
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2024-01-30 15:43:35 +0200

    Time: 688 ms (5.01%), Space: 46 MB (44.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,6 @@
 class Solution {
     public int trap(int[] height) {
-        int maxSoFar = height[0];
         int water = 0;
-        
-//         outer: for(int i = 1; i < height.length; i++){
-//             if(height[i] >= maxSoFar){
-//                 maxSoFar = height[i];
-//             }else{
-//                 boolean bool = false;
-//                 for(int j = i+1; j < height.length; j++){
-//                     if(height[j] >= maxSoFar){
-//                         water += maxSoFar - height[i];
-//                         continue outer;
-//                     }
-//                 }
-                
-//                 for(int j = i+1; j < height.length; j++){
-//                     if(height[j] >= height[i] && height[i-1] > height[i]){
-//                         water += height[i-1] - height[i];
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
         
         for(int i = 1; i < height.length; i++){
             int maxBefore = -1;
