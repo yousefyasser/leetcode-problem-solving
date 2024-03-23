434f8e674fb8fe355a23f16fef9ac85bf44d6f21
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2024-03-23 19:08:24 +0200

    Time: 4 ms (52.52%), Space: 9.6 MB (39.57%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+        
+        for(int i = 0; i <= n; i++){
+            int tmp = i;
+            int cnt = 0;
+            
+            while(tmp){
+                cnt += (tmp & 1);
+                tmp >>= 1;
+            }
+            
+            res.push_back(cnt);
+        }
+        
+        return res;
+    }
+};
