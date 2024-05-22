b595f1d2c397c7cba1cac3d7f94365ef20c28ca6
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2024-05-22 22:04:46 +0300

    Time: 600 ms (5.06%), Space: 11.5 MB (40.28%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    vector<bool> primes;
-    
     int countPrimes(int n) {
-        primes = vector<bool>(n, true);
-        sieve();
+        vector<bool> primes(n, true);
         
         int primesLessThanN = 0;
-        for(int i = 2; i < n; i++){
-            if(primes[i])  
-                primesLessThanN++;
-        }
-        
-        return primesLessThanN;
-    }
-    
-    void sieve(){
         for(int i = 2; i < primes.size(); i++){
             if(!primes[i])
                 continue;
             
+            primesLessThanN++;
             for(int j = 2*i; j < primes.size(); j += i){
                 primes[j] = false;
             }
         }
+        
+        return primesLessThanN;
     }
 };
